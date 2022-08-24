d66857c2d2f536484e709352040cd6ee297b17d2
conor.westphal <conor.westphal@ai-solutions.com>
2022-08-24 10:11:11 -0400

    some more progress

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <regex>
 #include <string>
 
+#include <SpiceUsr.h>
+
 bool is_valid_date_format(const std::string& input) {
    if (!std::regex_match(input, std::regex("[0-9]{4}-[A-Z]{3}-[0-9]{2}"))) {
       std::cout << "Error: input epoch does not match the required format." << std::endl;
@@ -23,6 +25,112 @@ bool PerformQuery(
              << occulterName << "-" << targetName
              << "' system in the time window between " << lowerEpoch << " and "
              << upperEpoch << "." << std::endl;
+
+   SpiceBoolean bail;
+   SpiceBoolean rpt;
+   SpiceChar*   win0;
+   SpiceChar*   win1;
+   SpiceChar    begstr[41];
+   SpiceChar    endstr[41];
+
+   SPICEDOUBLE_CELL(cnfine, 200);
+   SPICEDOUBLE_CELL(result, 200);
+
+   SpiceDouble et0;
+   SpiceDouble et1;
+   SpiceDouble left;
+   SpiceDouble right;
+
+   SpiceInt i;
+
+   furnsh_c("gfocce_ex1.tm");
+
+   win0 = "2030 JAN 01 00:00:00 TDB";
+   win1 = "2040 JAN 01 00:00:00 TDB";
+   str2et_c(win0, &et0);
+   str2et_c(win1, &et1);
+
+   wninsd_c(et0, et1, &cnfine);
+
+   gfsstp_c(20.0);
+
+   bail = SPICETRUE;
+   rpt  = SPICETRUE;
+
+   gfocce_c(
+      "ANY",
+      "MOON",
+      "ellipsoid",
+      "IAU_MOON",
+      "SUN",
+      "ellipsoid",
+      "IAU_SUN",
+      "LT",
+      "EARTH",
+      1.e-6,
+      gfstep_c,
+      gfrefn_c,
+      rpt,
+      gfrepi_c,
+      gfrepu_c,
+      gfrepf_c,
+      bail,
+      gfbail_c,
+      &cnfine,
+      &result);
+
+   if (gfbail_c()) {
+      /*
+      Clear the CSPICE interrupt indication. This is
+      an essential step for programs that continue
+      running after an interrupt;
+      gfbail_c will continue to return SPICETRUE until this step has been
+            performed.*/
+      gfclrh_c();
+
+      /*We've trapped an interrupt signal. In a realistic application,
+         the program
+            would continue operation from this point.In this simple example,
+         we simply display a message and                            quit.*/
+      std::cout << "\nSearch was interrupted.\n\nThis message "
+                   "was written after an interrupt signal\n"
+                   "was trapped. By default, the program "
+                   "would have terminated \nbefore this message "
+                   "could be written.\n\n";
+   }
+   else {
+
+      if (wncard_c(&result) == 0) {
+         std::cout << "No occultation was found.\n";
+      }
+      else {
+         for (i = 0; i < wncard_c(&result); i++) {
+            /*fetch and display each occultation interval.*/
+            wnfetd_c(&result, i, &left, &right);
+
+            timout_c(left, "YYYY MON DD HR:MN:SC.###### ::TDB (TDB)", 41, begstr);
+            timout_c(right, "YYYY MON DD HR:MN:SC.###### ::TDB (TDB)", 41, endstr);
+
+            std::cout << "Interval %d\n", (int)i;
+            std::cout << "   Start time: %s\n", begstr;
+            std::cout << "   Stop time:  %s\n", endstr;
+         }
+      }
+   }
+
+   /*auto          oName = static_cast<ConstSpiceChar*>(observerName.c_str());
+   SpiceInt*     oID(0);
+   SpiceBoolean* oFound(false);
+   bodn2c_c(oName, oID, oFound);
+   if (!oFound) {
+      std::cout << "Unable to find the NAIF ID corresponding to the specified observer."
+                << std::endl;
+      return false;
+   }
+   else {
+      std::cout << "The NAIF ID for the specified observer is: " << oID << std::endl;
+   }*/
+
    return true;
 }
 
